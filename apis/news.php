ed5bf49e35111895e89766c4015f333fb7f3bf96
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-23 13:39:00 +0300

    finalizing the php backend file

--- apis/news.php
+++ apis/news.php
@@ -4,11 +4,17 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
-
-$query = $mysqli->prepare("SELECT distinct(news-content, created_at) FROM news );
+$query = $connection->prepare("SELECT news_content, created_at FROM news");
 $query->execute();
 $array = $query->get_result();
+
 $response=[];
 
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
 
-?>
+$json = json_encode($response);
+echo $json;
+
+?>  
