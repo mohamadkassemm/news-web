1ed7f45304ad29e35b2e98dd7b85193aa7a0b2bb
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-23 16:47:23 +0300

    appending cards from database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-
+const $content= $('#content') 
     $.ajax({
         type: 'GET',
         URL: 'http://localhost/news-web/apis/news.php',
+        dataType : `json`,
         success: function(data){
             console.log('success', data);
+            $.each(data,function(data){
+                $content.append('<div class="row"> <div class="col-lg"> <h3 class="mb-4">News content</h3> <p>news:'data.news_content'</p> <p>published at:'data.created_at'</p></div></div>')
+            }) 
         }
     });
